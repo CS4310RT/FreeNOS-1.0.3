e99fd1d553c9d6c50a3599c5d8c08346de1081e1
unknown <timhswede@gmail.com>
2023-04-03 10:30:38 +0800

    Setup Renice files

---
+++ bin/renice/Renice.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015 Niek Linnenbank
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Types.h>
+#include <Macros.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ProcessClient.h>
+#include "Renice.h"
+
+Renice::Renice(int argc, char **argv)
+    : POSIXApplication(argc, argv)
+{
+    parser().setDescription("");
+    parser().registerFlag('l', "level", "Lists priority level of processes");
+}
+
+Renice::~Renice(){}
+
+Renice::Result Renice::exec()
+{
+    const ProcessClient process;
+
+    // Output the table
+    return Success;
+}
+
